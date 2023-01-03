ef232b82b2503c85d5598b464f2f97e7385281f6
aniawiki <annsie21@wp.pl>
2023-01-03 11:53:03 +0100

    initial commit

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,4 +1,4 @@
-var password = "Once in a blue moon";
+var password = sayings[Math.floor(Math.random() * sayings.length)];
 password = password.toUpperCase();
 
 var length = password.length;
@@ -21,32 +21,9 @@ function write_password() {
 window.onload = start;
 
 var letters = new Array(27);
-letters[0] = "A";
-letters[1] = "B";
-letters[2] = "C";
-letters[3] = "D";
-letters[4] = "E";
-letters[5] = "F";
-letters[6] = "G";
-letters[7] = "H";
-letters[8] = "I";
-letters[9] = "J";
-letters[10] = "K";
-letters[11] = "L";
-letters[12] = "M";
-letters[13] = "N";
-letters[14] = "O";
-letters[15] = "P";
-letters[16] = "Q";
-letters[17] = "R";
-letters[18] = "S";
-letters[19] = "T";
-letters[20] = "U";
-letters[21] = "V";
-letters[22] = "W";
-letters[23] = "X";
-letters[24] = "Y";
-letters[25] = "Z";
+
+for (i = 0; i < 27; i++)
+    letters[i] = String.fromCharCode(65 + i)
 
 function start() {
     var divv = "";
