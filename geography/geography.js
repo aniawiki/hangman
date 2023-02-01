f1788b055181d9e57d99f63e517b6def90b060fd
aniawiki <annsie21@wp.pl>
2023-02-01 21:42:57 +0100

    starting congrats

--- geography/geography.js
+++ geography/geography.js
@@ -1,22 +1,40 @@
+list = list1;
+var number, ptr, l, t, w, text, text2, node, d, time, time2, correct;
+node = document.getElementsByClassName("in")[0];
+ptr = document.getElementById('pointer');
+d = new Date();
+time = d.getTime();
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function gameOver() {
     ptr.style.visibility = "hidden";
-    setTimeout(function() { text.style.visibility = "hidden"; }, 1000);
+    setTimeout(function() { text.style.visibility = "hidden"; text2.style.visibility = "hidden"}, 1000);
+    d = new Date();
+    time2 = (d.getTime() - time) / 1000;
+    
+    console.log(time2);
 }
 
 function appearText(move) {
     text.style.visibility = "visible";
+    if (correct) text2.style.visibility = "hidden";
+    else text2.style.visibility = "visible";
     let s = 20;
     let l2 = 850;
+    var t = 450;
     for (let i = 0; i < 20; i++) {
         setTimeout(function() {
             s += 5;
+            t += 5;
             text.style.fontSize = s + "px";
+            text2.style.fontSize = s/2 + "px";
             l2 -= move;
             text.style.left = l2 + "px";
+            text2.style.left = l2 + "px";
+            text2.style.top = t + "px";
         }, 20*i);
     }
     setTimeout(function() { hideText(move); }, 1000);    
@@ -24,41 +42,48 @@ function appearText(move) {
 
 function hideText(move) {
     var s = 120
-    var l2 = 550;
+    var l2 = 850 - 20*move;
+    var t = 550;
     for (let i = 0; i < 20; i++) {
         setTimeout(function() {
             s -= 5;
+            t -= 5;
             text.style.fontSize = s + "px";
+            text2.style.fontSize = s/2 + "px";
             l2 += move;
             text.style.left = l2 + "px";
+            text2.style.left = l2 + "px";
+            text2.style.top = t + "px";
         }, 10*i);
     }
-    setTimeout(function() { text.style.visibility = "hidden"; }, 200);
+    setTimeout(function() { text.style.visibility = "hidden"; text2.style.visibility = "hidden"; }, 200);
     if (list.length > 0) setRandomCountry();
     else gameOver();
 }
 
-list = list1;
-var number, ptr, l, t, w, text, node;
-node = document.getElementsByClassName("in")[0];
-ptr = document.getElementById('pointer');
 
 node.addEventListener("keyup", function (event) {
     text = document.getElementById('txt');
+    text2 = document.getElementById('txt2');
     sleep(2000);
     var v = document.getElementById("in").value;
     if (event.key == "Enter") {
         if (v == list[number][0]) {
+            correct = true;
             text.innerHTML = "GOOD";
             text.style.width = "20px";
             appearText(5);
             list.splice(number, 1);
         }
         else {
+            correct = false;
             text.style.width = "1px";
             text.innerHTML = "NOT GOOD";
+            var message = "The correct answer is " + list[number][0];
+            text2.innerHTML = message;
             appearText(15);
         }
+        node.value = "";
     }
 });
 
