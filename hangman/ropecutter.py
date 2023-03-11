af2c8a1819d1c0a6fa3a5bd8bb4a280539fb2fbe
aniawiki <annsie21@wp.pl>
2023-03-11 22:21:20 +0100

    type pswd, check in dict, complete pattern

--- hangman/ropecutter.py
+++ hangman/ropecutter.py
@@ -3,19 +3,36 @@ import re
 
 
 def main():
-    global password, score
-    #freq = ["e", "a", "r", "i", "o", "t", "n", "s", "l", "c", "u", "d", "p", "m", "h", "g", "b", "f", "y", "w", "k", "v", "x", "z", "j", "q"]
+    global password
+    score = 0
+    freq = ["e", "a", "r", "i", "o", "t", "n", "s", "l", "c", "u", "d", "p", "m", "h", "g", "b", "f", "y", "w", "k", "v", "x", "z", "j", "q"]
     lines = open("dict.txt").read().splitlines()
     password = random.choice(lines)
     print(password)
+    l = len(password)
     pattern = str(len(password) * ".")
-    #pattern = re.compile(pattern)
-    print(pattern) 
+
     txt1 = open("dict.txt", "r")
     txt = txt1.read()
-    #print(txt)
-    x = re.findall(pattern, str(txt))
-    print(x)
+    
+    while (score != l):
+        a = input()
+        for i in range(l):
+            if (password[i] == a):
+                print(password[i])
+                score += 1
+                pat = pattern
+                pattern = ""
+                if (i != 0):
+                    pattern = pat[0:i]
+                pattern += a
+                if (i != l):
+                    pattern += pat[i+1:l]
+        print(pattern)
+        x = re.findall(str(pattern), str(txt))
+        #print(x)
+        print()
+
     '''password = input().lower()
     for i in freq:
         if score != int(len(password)):
