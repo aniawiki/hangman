8fafdf04b8a30c5b2d5596c7c5a33ab782818a47
aniawiki <annsie21@wp.pl>
2023-03-11 21:13:37 +0100

    working

---
+++ hangman/ropecutter.py
@@ -0,0 +1,29 @@
+import random
+import re
+
+
+def main():
+    global password, score
+    #freq = ["e", "a", "r", "i", "o", "t", "n", "s", "l", "c", "u", "d", "p", "m", "h", "g", "b", "f", "y", "w", "k", "v", "x", "z", "j", "q"]
+    lines = open("dict.txt").read().splitlines()
+    password = random.choice(lines)
+    print(password)
+    pattern = str(len(password) * ".")
+    #pattern = re.compile(pattern)
+    print(pattern)
+    txt1 = open("dict.txt", "r")
+    txt = txt1.read()
+    #print(txt)
+    x = re.findall(pattern, str(txt))
+    print(x)
+    '''password = input().lower()
+    for i in freq:
+        if score != int(len(password)):
+            score += password.count(i)
+        else:
+            print(password)
+            break'''
+
+
+if __name__ == "__main__":
+    main()
